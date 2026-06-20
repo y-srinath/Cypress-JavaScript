4c3d15950e1c74af1abd724bea472df9330d0594
Srinath Y <sriyellepeddi@yahoo.com>
2026-06-20 16:11:05 -0400

    added command delays and timeout config for better test visibility

--- cypress.config.js
+++ cypress.config.js
@@ -5,6 +5,7 @@ module.exports = defineConfig({
 
   e2e: {
     watchForFileChanges: false,
+    defaultCommandTimeout: 10000,
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
--- cypress/e2e/First_Test.cy.js
+++ cypress/e2e/First_Test.cy.js
@@ -5,11 +5,16 @@ describe("SauceDemo Login", () => {
 
     cy.get('[data-test="username"]').type("standard_user");
     cy.get('[data-test="password"]').type("secret_sauce");
-
+    cy.wait(3000);
     cy.get('[data-test="login-button"]').click();
 
     cy.url().should("include", "/inventory.html");
-
     cy.get(".inventory_item").should("have.length.greaterThan", 0);
+
+    cy.get('[data-test="add-to-cart-sauce-labs-backpack"]').click();
+    cy.get(".shopping_cart_badge").should("contain", "1");
+    cy.wait(3000);
+
+    cy.get('[data-test="shopping-cart-link"]').click();
   });
 });
