f350e0847c577ec8d60152ae79823a2f08e9ba02
Srinath Yellepeddi <srinath_y@Srinaths-MacBook-Air.local>
2026-06-21 22:34:54 -0400

    Expanded API test coverage with POST, PUT, DELETE methods

--- cypress/e2e/API_Testing/HTTP_Requests.cy.js
+++ cypress/e2e/API_Testing/HTTP_Requests.cy.js
@@ -7,4 +7,37 @@ describe("HTTP Requests", () => {
       },
     );
   });
+
+  it("POST Request", () => {
+    cy.request("POST", "https://jsonplaceholder.typicode.com/posts", {
+      title: "foo",
+      body: "bar",
+      userId: 1,
+    }).then((response) => {
+      expect(response.status).to.eq(201);
+      expect(response.body).to.have.property("id");
+    });
+  });
+
+  it("PUT Request", () => {
+    cy.request("PUT", "https://jsonplaceholder.typicode.com/posts/1", {
+      id: 1,
+      title: "foo",
+      body: "bar",
+      userId: 1,
+    }).then((response) => {
+      expect(response.status).to.eq(200);
+      expect(response.body).to.have.property("title", "foo");
+    });
+  });
+
+  it("DELETE Request", () => {
+    cy.request("DELETE", "https://jsonplaceholder.typicode.com/posts/1").then(
+      (response) => {
+        expect(response.status).to.eq(200);
+      },
+    );
+  }); 
+
+
 });
